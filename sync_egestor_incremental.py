d96dffae4a2dfe0be8a156d3d51a9fa5ead06a84
Vagner Kroning <vagnerdesign@MacBook-Pro.local>
2026-03-24 07:06:14 -0300

    fix token env var

--- sync_egestor_incremental.py
+++ sync_egestor_incremental.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 # =========================
 # CONFIGURAÇÕES
 # =========================
-EGESTOR_PERSONAL_TOKEN = os.getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHAiOiIxMGQxNzEzNGJiNmNkMTlkNGQ3YmNkYzgwNTNlNjRjMSIsInN1YmRvbWluaW8iOiJwYWRhcmlha3JvbmluZyIsImNsaWVudCI6IjY3Nzg4NmMxNDdkZWRiNWI3OTI2M2ZjYTUzZDMzNWY1M2Q1YTRmNzMiLCJjcmVhdGVkIjoxNzcyOTk5MTI0fQ==.dUAdBdESSI7mtkTvMeBRRXPOo5dWHIjpZRWHZCVuk6I", "")
+EGESTOR_PERSONAL_TOKEN = os.getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHAiOiIxMGQxNzEzNGJiNmNkMTlkNGQ3YmNkYzgwNTNlNjRjMSIsInN1YmRvbWluaW8iOiJwYWRhcmlha3JvbmluZyIsImNsaWVudCI6IjY3Nzg4NmMxNDdkZWRiNWI3OTI2M2ZjYTUzZDMzNWY1M2Q1YTRmNzMiLCJjcmVhdGVkIjoxNzcyOTk5MTI0fQ==.dUAdBdESSI7mtkTvMeBRRXPOo5dWHIjpZRWHZCVuk6I=", "")
 SUPABASE_URL = os.getenv("https://grusczwscturplevobsv.supabase.co", "")
 SUPABASE_SERVICE_ROLE_KEY = os.getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdydXNjendzY3R1cnBsZXZvYnN2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MzcwNjQ2MCwiZXhwIjoyMDg5MjgyNDYwfQ.lpOXnPdkUI9sUWY28maWeJaKvfvu3rw780AIjwP_BSg", "")
 JANELA_DIAS_VENDAS = int(os.getenv("JANELA_DIAS_VENDAS", "7"))
